efb8216f2a1b2e2df864f848fbf43f481f6ea9ba
bfkss <github@thisis.totallymy.email>
2024-12-04 10:57:28 +0100

    3.2

--- 3/main.ts
+++ 3/main.ts
@@ -9,3 +9,21 @@ for (const match of matches) {
   sum += +match.groups!.a * +match.groups!.b
 }
 console.log(sum)
+
+const re2 =
+  /(mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)|(?<y>do\(\))|(?<n>don't\(\)))/gm
+const matches2 = input.matchAll(re2)
+let sum2 = 0
+let enabled = true
+for (const match of matches2) {
+  if (enabled && match.groups?.n) {
+    enabled = false
+  }
+  if (!enabled && match.groups?.y) {
+    enabled = true
+  }
+  if (enabled && match.groups?.a && match.groups?.b) {
+    sum2 += +match.groups.a * +match.groups.b
+  }
+}
+console.log(sum2)
