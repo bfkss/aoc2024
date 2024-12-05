9383701082cbdd75d2be1031d7d290d6862fc6c2
bfkss <github@thisis.totallymy.email>
2024-12-05 13:37:16 +0100

    further steps

--- 4/main.ts
+++ 4/main.ts
@@ -8,34 +8,57 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX`
-const parsedInput = input.split('\n')
+const lines = input.split('\n')
 
-const fw = /xmas/ig
-const bw = /samx/ig
-let count = 0
-// forward XMAS in a row
-for (const line of parsedInput) {
-  const matchFw = line.matchAll(fw)
-  const matchBw = line.matchAll(bw)
-  // if (matchFw || matchBw) {
-  //   console.log([...matchFw])
-  //   console.log([...matchBw])
-  // }
-  count += [...matchFw].length
-  count += [...matchBw].length
-}
-console.log(count)
-// flip matrix
-let flipped: string[][] = [[]]
-for (let l = 0; l < parsedInput.length; l++) {
-  const line = parsedInput[l].split('')
-  for (let c = 0; c < line.length; c++) {
-    flipped[c].push(line[c])
+function findXmas(text: string[]): number {
+  const fw = /xmas/gi
+  const bw = /samx/gi
+  let count = 0
+  // forward XMAS in a row
+  for (const line of text) {
+    const matchFw = line.matchAll(fw)
+    const matchBw = line.matchAll(bw)
+    // if (matchFw || matchBw) {
+    //   console.log([...matchFw])
+    //   console.log([...matchBw])
+    // }
+    count += [...matchFw].length
+    count += [...matchBw].length
   }
+  return count
 }
-console.log(flipped)
 
-// backward XMAS in a row
-// top-down
-// bottom-up
-// diagonal
+// lines
+console.log(findXmas(lines))
+
+// rows
+const flipped: string[] = []
+for (let i = 0; i < lines[0].length; i++) {
+  const newLine: string[] = []
+  for (let j = 0; j < lines.length; j++) {
+    newLine.push(lines[j][i])
+  }
+  flipped.push(newLine.join(''))
+}
+
+console.log(findXmas(flipped))
+
+// diagonal TL to BR
+const diag1: string[] = []
+for (let i = 0; i < lines[0].length; i++) {
+  const newDiagonal: string[] = []
+  for (let j = 0; j < lines.length; j++) {
+    newDiagonal.push(lines[j][])
+  }
+  diag1.push(newDiagonal.join(''))
+}
+// for (let i = 0; i < lines.length; i++) {
+//   const newDiagonal: string[] = []
+//   do {
+//     newDiagonal.push(lines[i][i % lines[i].length])
+//   } while (i % lines[i].length !== 0)
+//   diag1.push(newDiagonal)
+// }
+console.log(diag1)
+
+// diagonal TR to BL
