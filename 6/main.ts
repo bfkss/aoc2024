92392c3ace8d18f7bcae9c49a3d6e4a552bfd8bf
bfkss <github@thisis.totallymy.email>
2024-12-19 13:50:24 +0100

    6.1

--- 6/main.ts
+++ 6/main.ts
@@ -1,6 +1,8 @@
 const input = await Deno.readTextFile('./input')
 const parsedInput: string[] = input.split('\n')
 parsedInput.pop()
+const maxX = parsedInput[0].length - 1
+const maxY = parsedInput.length - 1
 
 const Direction = {
   North: '^',
@@ -39,12 +41,9 @@ for (let y = 0; y < parsedInput.length; y++) {
 
 console.log('starting at ', position)
 
-// return end position
-function walkUp() {
+function walkNorth() {
   for (let y = position.y; y >= 0; y--) {
-    const current = parsedInput[y][position.x]
     if (y === 0) {
-      // if (y === 0 && current !== '#') {
       position.y = y
       position.leaving = true
       break
@@ -55,16 +54,120 @@ function walkUp() {
         .split('')
         .toSpliced(position.x, 1, 'X')
         .join('')
-      parsedInput[y - 1] = parsedInput[y - 1]
-        .split('')
-        .toSpliced(position.x, 1, position.facing)
-        .join('')
     } else {
       position.y = y
       position.facing = Direction.East
+      // position.leaving = true
+      // parsedInput[position.y] = parsedInput[position.y]
+      //   .split('')
+      //   .toSpliced(position.x, 1, position.facing)
+      //   .join('')
       break
     }
   }
 }
-walkUp()
-console.log(position, parsedInput)
+
+function walkSouth() {
+  for (let y = position.y; y <= maxY; y++) {
+    if (y === maxY) {
+      position.y = y
+      position.leaving = true
+      break
+    }
+    const next = parsedInput[y + 1][position.x]
+    if (next !== '#') {
+      parsedInput[y] = parsedInput[y].split('').toSpliced(position.x, 1, 'X')
+        .join('')
+    } else {
+      position.y = y
+      position.facing = Direction.West
+      // position.leaving = true
+      // parsedInput[position.y] = parsedInput[position.y]
+      //   .split('')
+      //   .toSpliced(position.x, 1, position.facing)
+      //   .join('')
+      break
+    }
+  }
+}
+
+function walkEast() {
+  for (let x = position.x; x <= maxX; x++) {
+    if (x === maxX) {
+      position.x = x
+      position.leaving = true
+      break
+    }
+    const next = parsedInput[position.y][x + 1]
+    if (next !== '#') {
+      parsedInput[position.y] = parsedInput[position.y]
+        .split('')
+        .toSpliced(x, 1, 'X')
+        .join('')
+    } else {
+      position.x = x
+      position.facing = Direction.South
+      // position.leaving = true
+      // parsedInput[position.y] = parsedInput[position.y]
+      //   .split('')
+      //   .toSpliced(position.x, 1, position.facing)
+      //   .join('')
+      break
+    }
+  }
+}
+
+function walkWest() {
+  for (let x = position.x; x >= 0; x--) {
+    if (x === 0) {
+      position.x = x
+      position.leaving = true
+      break
+    }
+    const next = parsedInput[position.y][x - 1]
+    if (next !== '#') {
+      parsedInput[position.y] = parsedInput[position.y].split('').toSpliced(
+        x,
+        1,
+        'X',
+      ).join('')
+    } else {
+      position.x = x
+      position.facing = Direction.North
+      // position.leaving = true
+      // parsedInput[position.y] = parsedInput[position.y]
+      //   .split('')
+      //   .toSpliced(position.x, 1, position.facing)
+      //   .join('')
+      break
+    }
+  }
+}
+
+function* go() {
+  while (!position.leaving) {
+    yield walkNorth()
+    yield walkEast()
+    yield walkSouth()
+    yield walkWest()
+  }
+  parsedInput[position.y] = parsedInput[position.y]
+    .split('')
+    // .toSpliced(position.x, 1, position.facing)
+    .toSpliced(position.x, 1, 'X')
+    .join('')
+}
+go().forEach(() => {})
+// console.log(position, parsedInput)
+console.log('final position ', position)
+let distinct = 0
+for (const line of parsedInput) {
+  for (const char of line) {
+    if (char === 'X') {
+      distinct++
+    }
+  }
+}
+console.log(distinct)
+
+Deno.writeTextFile('./output', parsedInput.join('\n'))
